d2d030880313954e6aff5bf4e6c162e7b8be0bdb
graz <graz@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2001-10-01 11:48:04 +0000

    Added saving of preferences.

--- mov/src/org/mov/main/Analyser.java
+++ mov/src/org/mov/main/Analyser.java
@@ -1,22 +1,40 @@
 package org.mov.main;
 
 import java.awt.*;
-import java.io.InputStream;
+import java.awt.event.*;
+import java.io.*;
 import java.util.prefs.*;
 import javax.swing.*;
 
 import org.mov.util.*;
 
-public class Analyser extends JFrame {
+public class Analyser extends JFrame implements WindowListener {
     
     private static final String PREFS_FILE = "prefs.xml";
     private JDesktopPane desktop;
     private AnalyserMenu menu;
 
     public Analyser() {
+	loadPreferences();
+	setTitle("Venice");
+	Preferences p = Preferences.userRoot().node("/display");
+	setSize(p.getInt("default_width", 800),
+		p.getInt("default_height", 600));
+	setLocation(p.getInt("default_x", 0),
+		    p.getInt("default_y", 0));
+
+	desktop = new JDesktopPane();
+	menu = new AnalyserMenu(this, desktop);
+	setContentPane(desktop);
+	Progress.setDesktop(desktop);
+	addWindowListener(this);
+    }
+
+    private void loadPreferences() {
 	// Read in the system preferences
 	try {
-	    java.net.URL prefs_url = ClassLoader.getSystemClassLoader().getResource(PREFS_FILE);
+	    java.net.URL prefs_url = 
+		ClassLoader.getSystemClassLoader().getResource(PREFS_FILE);
 	    if (prefs_url != null) {
 		InputStream is = prefs_url.openStream();
 		Preferences.importPreferences(is);
@@ -33,18 +51,46 @@ public class Analyser extends JFrame {
 			       ":\n"+
 			       ipfe.getMessage());
 	}
-
-	setTitle("Venice");
-	Preferences p = Preferences.userRoot().node("/display");
-	setSize(p.getInt("default_width", 800),
-		p.getInt("default_height", 600));
-
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	desktop = new JDesktopPane();
-	menu = new AnalyserMenu(this, desktop);
-	setContentPane(desktop);
-	Progress.setDesktop(desktop);
     }
+
+    private void savePreferences() {
+	// Save the preferences
+	try {
+	    FileOutputStream os = new FileOutputStream(PREFS_FILE);
+	    Preferences p = Preferences.userRoot();
+	    p.exportSubtree(os);
+	    os.close();
+	} catch (java.io.IOException ioe) {
+	    System.err.println("IO Exception thrown while saving "+
+			       PREFS_FILE+
+			       ":\n"+
+			       ioe.getMessage());
+	} catch (java.util.prefs.BackingStoreException bse) {
+	    System.err.println("Trouble saving file "+
+			       PREFS_FILE+
+			       ":\n"+
+			       bse.getMessage());
+	}
+    }
+
+    public void windowActivated(WindowEvent e) {}
+    public void windowClosing(WindowEvent e) {
+	// Save window dimensions in prefs file
+	Preferences p = Preferences.userRoot().node("/display");
+	p.putInt("default_width", getWidth());
+	p.putInt("default_height", getHeight());
+	p.putInt("default_x", getX());
+	p.putInt("default_y", getY());
+
+	dispose();	
+	savePreferences();
+	System.exit(0);
+    }
+    public void windowClosed(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowOpened(WindowEvent e) {}
 
     public static void main(String[] args) {
 	Analyser analyser = new Analyser();

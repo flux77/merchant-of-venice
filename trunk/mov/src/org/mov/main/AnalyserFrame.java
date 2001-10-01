8c4271aa5d36a833d254d975a274fefe01ff43df
graz <graz@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2001-10-01 11:52:32 +0000

    Now calls modules just before the window closes giving them an opportunity to save preferences date etc.

--- trunk/mov/src/org/mov/main/AnalyserFrame.java
+++ trunk/mov/src/org/mov/main/AnalyserFrame.java
@@ -2,9 +2,10 @@ package org.mov.main;
 
 import java.beans.*;
 import javax.swing.*;
+import javax.swing.event.*;
 
 public class AnalyserFrame extends JInternalFrame
-    implements PropertyChangeListener
+    implements PropertyChangeListener, InternalFrameListener
 {
     // Property indicating window should be closed
     public static final String WINDOW_CLOSE_PROPERTY = "window close";
@@ -14,11 +15,15 @@ public class AnalyserFrame extends JInternalFrame
 
     private final static int DEFAULT_LAYER = 2;
 
+    private AnalyserModule module;
+
     public AnalyserFrame(AnalyserModule module,
 			 int x, int y, int width, int height) {
 
 	// Resizable, closable etc
 	super(module.getTitle(), true, true, true, true);
+
+	this.module = module;
 
 	// Module can be enclosed in scroll pane if it desires to be
 	if(module.encloseInScrollPane()) 
@@ -31,9 +36,13 @@ public class AnalyserFrame extends JInternalFrame
 	    setJMenuBar(module.getJMenuBar());
 
 	// Listen to events from module
-	module.addPropertyChangeListener(this);
+	module.addModuleChangeListener(this);
 
 	super.setFrameIcon(module.getFrameIcon());
+
+	// We want to notify module when it is closing so it can save data
+	addInternalFrameListener(this);
+
 	show();	
     }
 
@@ -43,5 +52,18 @@ public class AnalyserFrame extends JInternalFrame
 	if(property.equals(WINDOW_CLOSE_PROPERTY))
 	    dispose();
     }
+
+    public void internalFrameActivated(InternalFrameEvent e) { }
+    public void internalFrameClosed(InternalFrameEvent e) { 
+	module.save();
+    }
+    public void internalFrameClosing(InternalFrameEvent e) { }
+    public void internalFrameDeactivated(InternalFrameEvent e) { }
+    public void internalFrameDeiconified(InternalFrameEvent e) { }
+    public void internalFrameIconified(InternalFrameEvent e) { }
+    public void internalFrameOpened(InternalFrameEvent e) { }
+
+
+    
 }
 

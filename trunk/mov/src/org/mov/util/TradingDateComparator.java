3e17c50ea1e10d21c9ba091f67af5004f62a7aad
graz <graz@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2001-10-01 11:55:08 +0000

    Can now be used to sort both forwards and backwards.

--- trunk/mov/src/org/mov/util/TradingDateComparator.java
+++ trunk/mov/src/org/mov/util/TradingDateComparator.java
@@ -4,11 +4,23 @@ import java.util.*;
 
 public class TradingDateComparator implements Comparator {
     
+    public static int FORWARDS = 0;
+    public static int BACKWARDS = 1;
+
+    private int direction;
+
+    public TradingDateComparator(int direction) {
+	this.direction = direction;
+    }
+
     public int compare(Object o1, Object o2) {
 	TradingDate d1 = (TradingDate)o1;
 	TradingDate d2 = (TradingDate)o2;
-	
-	return(d2.compareTo(d1));
+
+	if(direction == BACKWARDS)
+	    return d2.compareTo(d1);
+	else
+	    return d1.compareTo(d2);
     }
     
     public boolean equals(Object o1, Object o2) {

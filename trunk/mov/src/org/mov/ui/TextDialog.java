cd52d5f982f662cb5f5dc0007f606a1388bcbc10
graz <graz@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2001-11-20 11:46:32 +0000

    Made it look like normal java dialogs.

--- trunk/mov/src/org/mov/ui/TextDialog.java
+++ trunk/mov/src/org/mov/ui/TextDialog.java
@@ -19,43 +19,61 @@ public class TextDialog implements ActionListener
     JPanel optionPanel;
 
     /**
-     * Default constructor
+     * Create new text dialog.
+     *
      * @param parent The parent component to tie the dialog to
      * @param message The question to ask the user
      * @param title The title to place on the dialog
      */
-    public TextDialog(JComponent parent, String message, String
-		      title)
+    public TextDialog(JComponent parent, String message, String title)
     {
+	newDialog(parent, message, title, "");
+    }
+
+    /**
+     * Create new text dialog.
+     *
+     * @param parent The parent component to tie the dialog to
+     * @param message The question to ask the user
+     * @param title The title to place on the dialog
+     * @param defaultText The default string to display in the text entry
+     */
+    public TextDialog(JComponent parent, String message, String title,
+    		      String defaultText) {
+	newDialog(parent, message, title, defaultText);
+    }
 	
+    // Create a new text dialog
+    private void newDialog(JComponent parent, String message, String title, 
+			   String defaultText) 
+    {
 	OKButton = new JButton("OK");
 	CancelButton = new JButton("Cancel");
 	DataTextField = new JTextField();
+	DataTextField.setText(defaultText);
+
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel(message);	    
+
 	BorderLayout layout = new BorderLayout();
 	layout.setHgap(50);
 	layout.setVgap(5);
 
-	optionPanel = new JPanel(layout);
+	panel.setLayout(layout);
+	panel.add(label, BorderLayout.NORTH);
+	panel.add(DataTextField, BorderLayout.CENTER);
 
-	optionPanel.add(DataTextField, BorderLayout.NORTH);
-	optionPanel.add(OKButton, BorderLayout.WEST);
-	optionPanel.add(CancelButton, BorderLayout.EAST);
 	OKButton.addActionListener (this);
 	CancelButton.addActionListener (this);
-	
-	Object options[] = new Object[]{optionPanel};
 
-
-	JOptionPane optionPane = new JOptionPane(
-						 message,
+	Object options[] = {OKButton, CancelButton};
+	JOptionPane optionPane = new JOptionPane(panel,
 						 JOptionPane.QUESTION_MESSAGE,
 						 JOptionPane.OK_CANCEL_OPTION,
 						 null, options, null);
-	/*	textDialog = optionPane.createDialog(parent,
-		title);
-	*/
+
 	textFrame = optionPane.createInternalFrame(parent,
-							   title);
+						   title);
 	optionPane.getRootPane().setDefaultButton(OKButton);
     }
     
@@ -80,14 +98,12 @@ public class TextDialog implements ActionListener
      */
     public void actionPerformed(ActionEvent e)
     {
-	if (e.getSource () == OKButton)
-	    {
-		option = DataTextField.getText();
-	    }
-	else if (e.getSource () == CancelButton)
-	    {
-		option = null;
-	    }
+	if (e.getSource () == OKButton) {
+	    option = DataTextField.getText();
+	}
+	else if (e.getSource () == CancelButton) {
+	    option = null;
+	}
 	textFrame.dispose();
     }
 }

b7c2f5044a2f7064873daa44a0ee9f3fd59a6ed1
graz <graz@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2001-11-24 05:47:02 +0000

    Fixed a bug where it wasnt letting you override the text on the progress window. It should allow it if the current maximum is <= 1.

--- mov/src/org/mov/util/Progress.java
+++ mov/src/org/mov/util/Progress.java
@@ -179,8 +179,9 @@ public class Progress {
 	    // If the progress window is already up, a sub-progress can
 	    // ONLY display its message if the parent process has a 0/1
 	    // status.
-	    if(maximum > 1)
+	    if(this.maximum <= 1) {
 		set(message, maximum);
+	    }
 
 	    return false;
 	}

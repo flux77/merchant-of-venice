f818135b9afb3a6b472214a29acb4fedb75736e1
graz <graz@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2002-05-23 11:36:22 +0000

    Now sorts stocks by approx most active. Also gives you ability to show/hide columns including new "equation slots" which will eventually display the results of equations run on the stock.

--- mov/src/org/mov/table/QuoteModule.java
+++ mov/src/org/mov/table/QuoteModule.java
@@ -12,12 +12,13 @@ import org.mov.main.*;
 import org.mov.util.*;
 import org.mov.parser.*;
 import org.mov.quote.*;
-import org.mov.ui.DesktopManager;
-import org.mov.ui.ProgressDialog;
-import org.mov.ui.ProgressDialogManager;
+import org.mov.ui.*;
 
 public class QuoteModule extends AbstractTable
-    implements Module {
+    implements Module,
+	       ActionListener {
+    
+    private static final int EQUATION_COLUMNS = 5;
 
     private static final int SYMBOL_COLUMN = 0;
     private static final int VOLUME_COLUMN = 1;
@@ -26,8 +27,22 @@ public class QuoteModule extends AbstractTable
     private static final int DAY_OPEN_COLUMN = 4;
     private static final int DAY_CLOSE_COLUMN = 5;
     private static final int CHANGE_COLUMN = 6;
+    private static final int ACTIVITY_COLUMN = 7;
+    private static final int EQUATION_SLOT_COLUMN = 8;
 
     private JMenuBar menuBar;
+    private JCheckBoxMenuItem showSymbolsColumn;
+    private JCheckBoxMenuItem showVolumeColumn;
+    private JCheckBoxMenuItem showDayLowColumn;
+    private JCheckBoxMenuItem showDayHighColumn;
+    private JCheckBoxMenuItem showDayOpenColumn;
+    private JCheckBoxMenuItem showDayCloseColumn;
+    private JCheckBoxMenuItem showChangeColumn;
+    private JCheckBoxMenuItem[] showEquationColumns =
+	new JCheckBoxMenuItem[EQUATION_COLUMNS];
+
+    private JMenuItem sortByMostActive;
+    private JMenuItem tableClose;
     
     private PropertyChangeSupport propertySupport;
     private QuoteCache cache;
@@ -37,11 +52,11 @@ public class QuoteModule extends AbstractTable
     class Model extends AbstractTableModel {
 	private String[] headers = {
 	    "Symbol", "Volume", "Day Low", "Day High", "Day Open", 
-	    "Day Close", "Change"};
+	    "Day Close", "Change", "Activity"};
 
 	private Class[] columnClasses = {
 	    String.class, Integer.class, String.class, String.class,
-	    String.class, String.class, Change.class};
+	    String.class, String.class, Change.class, Float.class};
 
 	private TradingDate date = null;
 	private QuoteCache cache;
@@ -62,15 +77,21 @@ public class QuoteModule extends AbstractTable
 	}
 
 	public int getColumnCount() {
-	    return headers.length;
+	    return headers.length + EQUATION_COLUMNS;
 	}
 	
 	public String getColumnName(int c) {
-	    return headers[c];
+	    if(c < headers.length) 
+		return headers[c];
+	    else
+		return new String("Eqn. " + (c + 1 - headers.length));
 	}
 
 	public Class getColumnClass(int c) {
-	    return columnClasses[c];
+	    if(c < headers.length) 
+		return columnClasses[c];
+	    else
+		return String.class;
 	}
 	
 	public Object getValueAt(int row, int column) {
@@ -111,6 +132,14 @@ public class QuoteModule extends AbstractTable
 			Converter.changeToChange
 			(cache.getQuote(symbol, Quote.DAY_OPEN, date),
 			 cache.getQuote(symbol, Quote.DAY_CLOSE, date));
+
+		// This column is never visible but is used to determine
+		// the most active stocks
+		case(ACTIVITY_COLUMN):
+		    return new Float(cache.getQuote(symbol, 
+						    Quote.DAY_HIGH, date) *
+				     cache.getQuote(symbol, 
+						    Quote.DAY_VOLUME, date));
 		}
 	    }
 	    catch(EvaluationException e) {
@@ -128,9 +157,6 @@ public class QuoteModule extends AbstractTable
     public QuoteModule(QuoteCache cache, 
 		      org.mov.parser.Expression expression) {
 	
-	menuBar = new JMenuBar();
-	menuBar.add(new JMenu("Test menu 1"));
-	menuBar.add(new JMenu("Test menu 2"));
 	this.expression = expression;
 	this.cache = cache;
 
@@ -141,7 +167,18 @@ public class QuoteModule extends AbstractTable
 	    symbols = cache.getSymbols();
 
 	propertySupport = new PropertyChangeSupport(this);
-	setModel(new Model(this.cache, symbols));
+	setModel(new Model(this.cache, symbols), ACTIVITY_COLUMN, SORT_UP);
+
+	addMenu();
+
+	// Set menu items to hide equation slots
+	for(int i = 0; i < EQUATION_COLUMNS; i++) {
+	    showColumn(EQUATION_SLOT_COLUMN + i, false);
+	}
+	
+	// Activity column is always hidden - its just used for
+	// sorting purposes
+	showColumn(ACTIVITY_COLUMN, false);
 
 	// If the user double clicks on a cell then graph the stock
 	addMouseListener(new MouseAdapter() {
@@ -198,7 +235,7 @@ public class QuoteModule extends AbstractTable
 
 	    // Tell user expression didnt evaluate properly
 	    JOptionPane.
-		showInternalMessageDialog(DesktopManager.getDesktop(),
+		showInternalMessageDialog(org.mov.ui.DesktopManager.getDesktop(),
 					  e.getReason() + ": " +
 					  expression.toString(),
 					  "Error evaluating expression",
@@ -213,6 +250,66 @@ public class QuoteModule extends AbstractTable
 	finally {
 	    ProgressDialogManager.closeProgressDialog();
 	}
+    }
+
+    private void addMenu() {
+	menuBar = new JMenuBar();
+
+	JMenu tableMenu = MenuHelper.addMenu(menuBar, "Table");
+
+	JMenu columnMenu = 
+	    MenuHelper.addMenu(tableMenu, "Show Columns");
+	{
+	    showSymbolsColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Symbols");
+	    showVolumeColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Volume");
+	    showDayLowColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Day Low");
+	    showDayHighColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Day High");
+	    showDayOpenColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Day Open");
+	    showDayCloseColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Day Close");
+	    showChangeColumn = 
+		MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+					       "Change Column");
+	    columnMenu.addSeparator();
+
+	    // Set menu items to hide equation slots
+	    for(int i = 0; i < EQUATION_COLUMNS; i++) {
+		showEquationColumns[i] = 
+		    MenuHelper.addCheckBoxMenuItem(this, columnMenu,
+						   "Equation Slot " + (i + 1));
+	    }
+
+	    // Put ticks next to the columns that are visible
+	    showSymbolsColumn.setState(true);
+	    showVolumeColumn.setState(true);
+	    showDayLowColumn.setState(true);
+	    showDayHighColumn.setState(true);
+	    showDayOpenColumn.setState(true);
+	    showDayCloseColumn.setState(true);
+	    showChangeColumn.setState(true);
+	}
+       
+	tableMenu.addSeparator();
+
+	sortByMostActive = 
+	    MenuHelper.addMenuItem(this, tableMenu,
+				   "Sort by Most Active");
+	
+	tableMenu.addSeparator();
+
+	tableClose = MenuHelper.addMenuItem(this, tableMenu,
+					    "Close");	
     }
 
     public void save() {
@@ -275,6 +372,64 @@ public class QuoteModule extends AbstractTable
      */
     public boolean encloseInScrollPane() {
 	return true;
+    }
+
+    /**
+     * Handle widget events.
+     *
+     * @param	e	action event
+     */
+    public void actionPerformed(final ActionEvent e) {
+	if(e.getSource() == tableClose) {
+	    propertySupport.
+		firePropertyChange(ModuleFrame.WINDOW_CLOSE_PROPERTY, 0, 1);
+	}
+	
+	else if(e.getSource() == sortByMostActive) {
+	    setColumnSortStatus(ACTIVITY_COLUMN, SORT_UP);
+	    resort();
+	    validate();
+	    repaint();
+	}
+
+	else {
+	    // Otherwise its a checkbox menu item
+	    JCheckBoxMenuItem menuItem = (JCheckBoxMenuItem)e.getSource();
+	    boolean state = menuItem.getState();
+	    int column = SYMBOL_COLUMN;
+
+	    if(menuItem == showSymbolsColumn) 
+		column = SYMBOL_COLUMN;
+
+	    else if(menuItem == showVolumeColumn) 
+		column = VOLUME_COLUMN;
+
+	    else if(menuItem == showDayLowColumn) 
+		column = DAY_LOW_COLUMN;
+
+	    else if(menuItem == showDayHighColumn) 
+		column = DAY_HIGH_COLUMN;
+
+	    else if(menuItem == showDayOpenColumn) 
+		column = DAY_OPEN_COLUMN;
+
+	    else if(menuItem == showDayCloseColumn) 
+		column = DAY_CLOSE_COLUMN;
+
+	    else if(menuItem == showChangeColumn) 
+		column = CHANGE_COLUMN;
+
+	    // Otherwise its an equation slot column
+	    else {
+		for(int i = 0; i < EQUATION_COLUMNS; i++) {
+		    if(menuItem == showEquationColumns[i]) {
+			column = EQUATION_SLOT_COLUMN + i;
+		    }
+		}
+	    }
+
+	    showColumn(column, state);
+	}
     }
     
 }

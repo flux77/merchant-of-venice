9c6cf874d927d0cd6ff5dd67ffdbd73ea84deecc
aleppard <aleppard@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2003-05-28 12:47:16 +0000

    Added support for variables to the gondola language. Also added the keywords "true" and "false" for the boolean values.

--- mov/src/org/mov/parser/Parser.java
+++ mov/src/org/mov/parser/Parser.java
@@ -23,127 +23,7 @@ import java.util.*;
 /**
  * Parse a string into an executable expression. This class acts as
  * a gatekeeper to the <i>Gondala</i> language which is used to perform
- * manipulations on stock market quotes. This language was specially
- * created to deal with quote objects and thus has some special features:
- * <p>
- * <ul><li>Inbuilt stock market types such as <i>volume</i> & <i>price</i>
- *         etc</li>
- *     <li>Built in functions to manipulate stock market quotes such as
- *	   <i>lag, min, max, avg</i> & <i>rsi</i>
- *     <li>Internal variables holding current quote and latest date being
- *         referenced</li>
- * </ul>
- * <p>
- * <h2>Language summary</h2>
- * <p>
- *
- * <p>
- * <h2>Functions</h2>
- * <p>
- *
- * <ul>
- * <li>
- * Average: <i>avg(quote, days, offset)</i>
- * <p>
- * Calculate the average stock quote over a period. For example to find
- * the average day open of the current stock over 5 days with the last day
- * being yesterday:
- * <p>
- * <code>avg(day_open, 5, -1)</code>
- * </li>
- *
- * <li>
- * Lag: <i>lag(quote, offset)</i>
- * <p>
- * Find the quote price for a stock on the given day. For example to find
- * the trading volume of the current stock today:
- * <p>
- * <code>lag(day_volume, 0)</code>
- * </li>
- *
- * <li>
- * Minimum Quote: <i>min(quote, days, offset)</i>
- * <p>
- * Calculate the minimum stock quote over a period. For example to find
- * the minimum day close of the current stock over 100 days with the last day
- * being yesterday:
- * <p>
- * <code>min(day_close, 100, -1)</code>
- * </li>
- *
- * <li>
- * Maximum Quote: <i>max(quote, days, offset)</i>
- * <p>
- * Calculate the maximum stock quote over a period. For example to find
- * the maximum day high of the current stock over 100 days with the last day 
- * being 7 trading ago:
- * <p>
- * <code>max(day_high, 100, -7)</code>
- * </li>
- *
- * <li>
- * Percent: <i>percent(value, percent)</i>
- * <p>
- * Calculate <i>percent</i> percent of the value and return. For example to
- * find 10% of the volume traded on a single day:
- * <p>
- * <code>percent(lag(day_volume, 0), 10)</code>
- * </li>
- *
- * </ul>
- * <p>
- * <h2>Operators</h2>
- * <p>
- *
- * <ul>
- *
- * <li>
- * Boolean operators: <i>x and y, not(x), x or y</i>
- * <p>
- * The standard boolean operators. 
- * </li>
- *
- * <li>
- * Mathematical operators: <i>x + y, x - y, x * y, x / y</i>
- * <p>
- * The standard mathematical operators.
- * </li>
- * 
- * <li>
- * Relational operators: <i>x == y, x < y, x <= y, x > y, x >= y, x != y</i>
- * <p>
- * The standard relational operators.
- * </li>
- *
- * </ul>
- *
- * <p>
- * <h2>Control Statements</h2>
- * <p>
- * <ul>
- * <li>
- * If statement: <i>if(condition) { ... } else { ... } </i>
- * <p>
- * Standard <i>if</i> statement as you would find in any language.
- * </li>
- *
- * </ul>
- * <h2>Examples</h2>
- * <p>
- * <ul>
- * <li>Returns true if the current stock has traded more than
- * $100 Million today (sort of):
- * <p><code>lag(day_volume, 0) * lag(day_close, 0) > 100000000</code></li>
- *
- * <li>Returns true if the current stock has gained more than 10%:
- * <p><code>
- * (100 * (lag(day_close, 0) - lag(day_open, 0))) / lag(day_open, 0) > 10
- * </code></li>
- *
- * <li>Returns true if the current stock has reached a 30 day high:
- * <p><code>lag(day_high, 0) > max(day_high, 29, -1)</code></li>
- *
- * </ul>
+ * manipulations on stock market quotes. 
  * <p>
  * <h2>Langauge EBNF</h2>
  * <pre>
@@ -156,8 +36,8 @@ import java.util.*;
  * MULTIPLY_EXPR = FACTOR [ MULTIPLY_OPERATOR FACTOR ]
  * MULTIPLY_OPERATOR = "*" | "/"
  * FACTOR = VARIABLE | NUMBER | FUNCTION | "(" EXPR ")"
- * NUMBER = ["-"]{0-9}+ ["." {0-9}+]
- * VARIABLE = "held" | "age" 
+ * NUMBER = ["-"]{0-9}+ ["." {0-9}+] | "true" | "false"
+ * VARIABLE = {a-zA-Z}{a-zA-Z0-9}*
  * QUOTE      "day_open" | "day_close" | "day_low" | "day_high" | 
  *            "day_volume"
  * FUNCTION = "lag" "(" QUOTE "," EXPR ")" | 
@@ -169,17 +49,19 @@ import java.util.*;
  *            "percent" "(" EXPR "," EXPR ")" |
  *            "if"  "(" EXPR ")" "{" EXPR "}" "else" "{" EXPR "}"
  * </pre>
- * <h2>Symbols to be implemented</h2>
- * <p><i>age, held, today, yesterday</i>
- * 
  */
 public class Parser {
 
-    public Expression parse(String string) throws ExpressionException
-    {
+    private Parser() {
+        // class should not be instantiated
+    }
+
+    public static Expression parse(Variables variables, String string)
+        throws ExpressionException {
+
 	// Perform lexical analysis on string - i.e. reduce it to stack of
 	// tokens
-	TokenStack tokens = lexicalAnalysis(string);
+	TokenStack tokens = lexicalAnalysis(variables, string);
 
 	// Translate stack of tokens to expression
 	Expression expression = parseExpression(tokens);
@@ -190,7 +72,12 @@ public class Parser {
 	return expression;
     }
 
-    private TokenStack lexicalAnalysis(String string) 
+    public static Expression parse(String string) throws ExpressionException
+    {
+        return parse(new Variables(), string);
+    }
+
+    private static TokenStack lexicalAnalysis(Variables variables, String string) 
 	throws ParserException {
 
 	TokenStack tokens = new TokenStack();
@@ -207,7 +94,7 @@ public class Parser {
 
 		// Extract next token
 		token = new Token();
-		string = Token.stringToToken(token, string);
+		string = Token.stringToToken(variables, token, string);
 		tokens.add(token);
 	    }
 	}
@@ -215,7 +102,7 @@ public class Parser {
 	return tokens;
     }
 
-    private Expression parseExpression(TokenStack tokens) 
+    private static Expression parseExpression(TokenStack tokens) 
 	throws ParserException {
 
 	Expression left = parseBooleanExpression(tokens);
@@ -231,7 +118,7 @@ public class Parser {
 	return left;
     }
 
-    private Expression parseBooleanExpression(TokenStack tokens) 
+    private static Expression parseBooleanExpression(TokenStack tokens) 
 	throws ParserException {
 
 	Expression left = parseAddExpression(tokens);
@@ -251,7 +138,7 @@ public class Parser {
 	return left;
     }
 	
-    private Expression parseAddExpression(TokenStack tokens) 
+    private static Expression parseAddExpression(TokenStack tokens) 
 	throws ParserException {
 
 	Expression left = parseMultiplyExpression(tokens);
@@ -267,7 +154,7 @@ public class Parser {
 	return left;
     }
     
-    private Expression parseMultiplyExpression(TokenStack tokens) 
+    private static Expression parseMultiplyExpression(TokenStack tokens) 
 	throws ParserException {
 	
 	Expression left = parseFactor(tokens);
@@ -284,20 +171,16 @@ public class Parser {
 	return left;
     }	
 
-    private Expression parseFactor(TokenStack tokens) 
+    private static Expression parseFactor(TokenStack tokens) 
 	throws ParserException {
 
 	Expression expression;
 
-	// VARIABLE
-	if(tokens.match(Token.HELD_TOKEN) ||
-	   tokens.match(Token.AGE_TOKEN)) {
-	    expression = parseVariable(tokens);
-	}
-
 	// NUMBER
-	else if(tokens.match(Token.NUMBER_TOKEN) || 
-		tokens.match(Token.SUBTRACT_TOKEN)) {
+	if(tokens.match(Token.NUMBER_TOKEN) || 
+           tokens.match(Token.TRUE_TOKEN) || 
+           tokens.match(Token.FALSE_TOKEN) || 
+           tokens.match(Token.SUBTRACT_TOKEN)) {
 	    expression = parseNumber(tokens);
 	}
 	
@@ -319,31 +202,30 @@ public class Parser {
 	    expression = parseExpression(tokens);
 	    parseRightParenthesis(tokens);
 	}
+
+	// VARIABLE
+        else if(tokens.match(Token.VARIABLE_TOKEN))
+	    expression = parseVariable(tokens);
+
 	else
 	    throw new ParserException("unexpected symbol");
 
 	return expression;
     }
 
-    private Expression parseVariable(TokenStack tokens) 
+    private static Expression parseVariable(TokenStack tokens) 
 	throws ParserException {
 
 	Token variable = tokens.pop();
 	Expression expression;
-	
-	switch(variable.getType()) {
-	case(Token.HELD_TOKEN):
-	case(Token.AGE_TOKEN):
-	    expression = ExpressionFactory.newExpression(variable);
-	    break;
-	default:
-	    throw new ParserException("expected variable");
-	}
 
-	return expression;
+        if(variable.getType() == Token.VARIABLE_TOKEN) 
+            return ExpressionFactory.newExpression(variable);
+        else
+            throw new ParserException("unexpected symbol");
     }
 
-    private Expression parseQuote(TokenStack tokens) 
+    private static Expression parseQuote(TokenStack tokens) 
 	throws ParserException {
 	
 	Token quote = tokens.pop();
@@ -364,29 +246,34 @@ public class Parser {
 	return expression;
     }
 
-    private Expression parseNumber(TokenStack tokens) 
+    private static Expression parseNumber(TokenStack tokens) 
 	throws ParserException {
 
 	Token number = tokens.pop();
 	boolean negate = false;
 
-	// Is there a "-" infront? Handle negative numbers 
-	if(number.getType() == Token.SUBTRACT_TOKEN) {
-	    number = tokens.pop();
-	    negate = true;
-	}
+        if(number.getType() == Token.TRUE_TOKEN ||
+           number.getType() == Token.FALSE_TOKEN)
+            return ExpressionFactory.newExpression(number);
 
-	if(number.getType() == Token.NUMBER_TOKEN) {
-	    if(negate)
-		number.negate();
-
-	    return ExpressionFactory.newExpression(number);	    
-	}
-	else
-	    throw new ParserException("expected number");
+        else {
+            // Is there a "-" infront? Handle negative numbers 
+            if(number.getType() == Token.SUBTRACT_TOKEN) {
+                number = tokens.pop();
+                negate = true;
+            }
+            
+            if(number.getType() == Token.NUMBER_TOKEN) {
+                if(negate)
+                    number.negate();
+                return ExpressionFactory.newExpression(number);	    
+            }
+            else
+                throw new ParserException("expected number");
+        }
     }
 
-    private Expression parseFunction(TokenStack tokens) 
+    private static Expression parseFunction(TokenStack tokens) 
 	throws ParserException {
 
 	Expression expression;
@@ -483,36 +370,36 @@ public class Parser {
 	return expression;
     }
 
-    private void parseComma(TokenStack tokens) throws ParserException {
+    private static void parseComma(TokenStack tokens) throws ParserException {
 	if(!tokens.pop(Token.COMMA_TOKEN))
 	    throw new ParserException("expected comma");
     }
 
-    private void parseLeftParenthesis(TokenStack tokens) 
+    private static void parseLeftParenthesis(TokenStack tokens) 
 	throws ParserException {
 	if(!tokens.pop(Token.LEFT_PARENTHESIS_TOKEN))
 	    throw new ParserException("expected left parenthesis");
     }
 
-    private void parseRightParenthesis(TokenStack tokens) 
+    private static void parseRightParenthesis(TokenStack tokens) 
 	throws ParserException {
 	if(!tokens.pop(Token.RIGHT_PARENTHESIS_TOKEN))
 	    throw new ParserException("missing right parenthesis");
     }
 
-    private void parseLeftBrace(TokenStack tokens) throws ParserException {
+    private static void parseLeftBrace(TokenStack tokens) throws ParserException {
 	if(!tokens.pop(Token.LEFT_BRACE_TOKEN))
 	    throw new ParserException("expected left brace");
     }
 
-    private void parseRightBrace(TokenStack tokens) 
+    private static void parseRightBrace(TokenStack tokens) 
 	throws ParserException {
 
 	if(!tokens.pop(Token.RIGHT_BRACE_TOKEN))
 	    throw new ParserException("missing right brace");
     }
 
-    private void parseElse(TokenStack tokens) throws ParserException {
+    private static void parseElse(TokenStack tokens) throws ParserException {
 	if(!tokens.pop(Token.ELSE_TOKEN))
 	    throw new ParserException("expected else token");
     }

629c0dc1e33c4cb07c7b2814839c4a807caa3a43
aleppard <aleppard@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2003-08-19 10:09:52 +0000

    Now works!

--- trunk/mov/src/org/mov/ui/EquationComboBox.java
+++ trunk/mov/src/org/mov/ui/EquationComboBox.java
@@ -5,21 +5,23 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package org.mov.ui;
 
 import org.mov.prefs.*;
 
+import java.awt.Point;
+import java.awt.Component;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
@@ -43,16 +45,18 @@ public class EquationComboBox extends JComboBox
     // the user in the preferences page or elsewhere
     static HashMap storedEquations;
 
+    private boolean isDialogUp = false;
+
     /**
      * Create a new equation combo box.
      */
     public EquationComboBox() {
-	this(new String(""));
+	this("");
     }
 
     /**
      * Create a new equation combo box with the given default equation
-     * text. 
+     * text.
      *
      * @param	equationText	equation text to display
      */
@@ -72,9 +76,9 @@ public class EquationComboBox extends JComboBox
 	setEquationText(equationText);
 
         // The combo box must be big enough to hold this text. This makes it
-        // as wide as the equation combo box. Yes but on 1.4.1 it makes them short!      
+        // as wide as the equation combo box. Yes but on 1.4.1 it makes them short!
         //	setPrototypeDisplayValue("avg(day_close, 15, 15) > 121");
- 
+
 	// We want to know just before the popup items become visible
 	// so we can update them
 	addPopupMenuListener(this);
@@ -82,8 +86,20 @@ public class EquationComboBox extends JComboBox
 	// We want to know when the combo box loses focus. We need this
 	// to remember the last few equations that the user typed in.
 	addFocusListener(this);
+
+        // We want to catch right mouse buttons on the text field
+        for(int i = 0; i < getComponentCount(); i++) {
+            Component component = getComponent(i);
+
+            if(component instanceof JTextField)
+                component.addMouseListener(new MouseAdapter() {
+                        public void mouseClicked(MouseEvent event) {
+                            handleMouseClicked(event);
+                        }
+                    });
+        }
    }
-    
+
     /**
      * Return the equation string in the ComboBox. If a name of an
      * equation is in the box then its equation will be returned.
@@ -99,8 +115,19 @@ public class EquationComboBox extends JComboBox
 	// well return the actual equation - not its name
 	if(storedEquations.containsKey(text))
 	    return (String)storedEquations.get(text);
-	else 
+	else
 	    return text;
+    }
+
+    /**
+     * Return whether the current displayed equation is a stored equation.
+     * A stored equation is one the user has entered and can refer
+     * to by using a keyword.
+     *
+     * @return <code>true</code> if it is a stored equation.
+     */
+    public boolean isStoredEquation() {
+        return storedEquations.containsKey(getSelectedItem());
     }
 
     /**
@@ -174,5 +201,65 @@ public class EquationComboBox extends JComboBox
 
     public void focusLost(FocusEvent e) {
 	System.out.println("lost: equation is " + getEquationText());
+    }
+
+    private void showEditDialog() {
+        Thread thread = new Thread(new Runnable() {
+                public void run() {
+                    if(!isDialogUp) {
+                        isDialogUp = true;
+
+                        String equationText = getEquationText();
+                        String newEquationText;
+
+                        newEquationText = 
+                            ExpressionEditorDialog.showEditDialog("Edit Expression",
+                                                                  equationText);
+
+                        setEquationText(newEquationText);
+                        isDialogUp = false;
+                    }
+                }});
+                                   
+        thread.start();
+    }
+
+    private void handleMouseClicked(final MouseEvent event) {
+
+        // Right click on the table - raise menu
+        if(event.getButton() == MouseEvent.BUTTON3) {
+            JPopupMenu menu = new JPopupMenu();
+            
+            JMenuItem editMenuItem = new JMenuItem("Edit");
+            editMenuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        showEditDialog();
+                    }});
+            
+            menu.add(editMenuItem);
+
+            /*            
+            boolean isStoredEquation = isStoredEquation();
+
+            JMenuItem addMenuItem = new JMenuItem("Add");
+            addMenuItem.setEnabled(!isStoredEquation);
+            addMenuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        
+                    }});
+            menu.add(addMenuItem);
+            
+            JMenuItem deleteMenuItem = new JMenuItem("Delete");
+            deleteMenuItem.setEnabled(isStoredEquation);
+            deleteMenuItem.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        
+                    }});
+            menu.add(deleteMenuItem);
+            */              
+          
+            Point point = event.getPoint();
+            menu.show(this, point.x, point.y);
+        }
     }
 }

03cddb00be00c560d55e28dc7d3df5acd429e644
aleppard <aleppard@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2004-05-16 03:32:28 +0000

    Added RSI Graph.

--- trunk/mov/src/org/mov/quote/QuoteFunctions.java
+++ trunk/mov/src/org/mov/quote/QuoteFunctions.java
@@ -5,15 +5,15 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package org.mov.quote;
@@ -33,7 +33,7 @@ public class QuoteFunctions {
      * algorthim will calculate the standard deviation on the values in
      * the given array in the range [start, end]. Start inclusive, end exclusive.
      *
-     * @param values array of values to analyse 
+     * @param values array of values to analyse
      * @param start  analyse values from start
      * @param end    to end
      * @return the standard deviation
@@ -55,7 +55,7 @@ public class QuoteFunctions {
      * algorthim will calculate the average on the values in
      * the given array in the range [start, end]. Start inclusive, end exclusive.
      *
-     * @param values array of values to analyse 
+     * @param values array of values to analyse
      * @param start  analyse values from start
      * @param end    to end
      * @return the average
@@ -76,60 +76,77 @@ public class QuoteFunctions {
     }
 
     /**
-     * RSI algorithm 
-     * @param	quoteBundle	the quote cache to read the quotes from.
-     * @param	symbol	the symbol to use.
-     * @param	quote	the quote type we are interested in, e.g. DAY_OPEN.
-     * @param	days	Number of days to run RSI calculation over
-     * @param	lastDay	fast access date offset in cache.
-     * @return  the RSI value
+     * Calculate the Relative Strength Indicator (RSI) value. Technical Analysis
+     * by Martin J. Pring describes the RSI as:
+     *
+     * "It is a momentum indicator, or oscillator, that measures the relative internal
+     *  strength of a security against <i>itself</i>....".
+     *
+     * The formula for the RSI is as follows:
+     *
+     *               100
+     * RSI = 100 - ------
+     *             1 + RS
+     *
+     *       average of x days' up closes
+     * RS = ------------------------------
+     *      average of x days' down closes
+     *
+     * To calculate an X day RSI you need X + 1 quote values. So try and make
+     * sure that <code>start</code> is always greater than zero.
+     *
+     * @param values array of values to analyse
+     * @param start  analyse values from start
+     * @param end    to end
+     * @return RSI
      */
-    static public double rsi(QuoteBundle quoteBundle, Symbol symbol, 
-			    int quote, int days, int lastDay) {
-	System.err.println("Entering RSI for symbol "+symbol);
+    static public double rsi(double[] values, int start, int end) {
+        double sumGain = 0;
+        double sumLoss = 0;
+        int numberGains = 0;
+        int numberLosses = 0;
+        double previous;
 
-	//	Vector v = new Vector();
+        if(start > 0)
+            previous = values[start - 1];
+        else
+            previous = values[start];
 
-	// Determine the average up and down values for the days, divide by <period>
-	double upvalues   = 0;
-	double downvalues = 0;
-	
-	double last = 0;
-	double current;
-	for(int i = lastDay - days + 1; i <= lastDay; i++) {
-	    //	for(int i = 0; i < v.size(); i++) {
+        // Calculate average day up and down closes
+        for(int i = start; i < end; i++) {
+            if(values[i] > previous) {
+                sumGain += (values[i] - previous);
+                numberGains++;
+            }
 
-	    try {
-		current = quoteBundle.getQuote(symbol, quote, i);
-		System.err.println("offset "+i+", value "+current);
-		if (i < 0) {
-		    if (current > last)
-			upvalues += current;
-		    else if (current < last)
-			downvalues += current;
-		}
-		last = current;
-	    }
-	    catch(MissingQuoteException e) {
-		// ignore
-	    }
-	}
+            else if(values[i] < previous) {
+                sumLoss += (previous - values[i]);
+                numberLosses++;
+            }
 
-	double up_average = upvalues / days;
-	double down_average = downvalues / days;
-	System.out.println(" up: "+up_average+
-			   " down: "+down_average);
-	// RS = (up average / down average) + 1
-	double strength = (up_average / down_average) + 1;
-	System.err.println("s1: "+strength);
-	// N = 100 / RS
-	strength = 100 / strength;
-	
-	// RSI = 100 - N
-	strength = 100 - strength;
-	System.err.println("Value: "+strength);
-	return strength;
+            previous = values[i];
+        }
+
+        // If the period is too small, return a neutral result
+        if(numberLosses == 0 && numberGains == 0)
+            return 50.0D;
+
+        // If avg loss is 0, then RSI returns 100 by definition.
+        else if(numberLosses == 0 || (sumLoss / numberLosses == 0.0D))
+            return 100.0D;
+
+        else {
+            double avgGain;
+            double avgLoss = sumLoss / numberLosses;
+
+            if (numberGains == 0)
+                avgGain = 0;
+            else
+                avgGain = sumGain / numberGains;
+
+            double RS = avgGain / avgLoss;
+            return 100.0D - 100.D / (1.0D + RS);
+        }
     }
-
 }
 

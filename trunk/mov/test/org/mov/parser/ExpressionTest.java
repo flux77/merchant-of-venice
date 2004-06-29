184a3067830a4fe70cf242dc852a3a446fa6d509
aleppard <aleppard@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2004-06-29 10:33:24 +0000

    I've got the unit tests working again!
    There was a problem with an earlier version of JUnit/Ant were they
    simply did not play well together and I had to abandom the tests.
    It looks like this has all been fixed now.
    
    There were a few minor interface changes that required modifications to
    build the tests, and overall all the tests still worked. The only
    problem was that ClauseExpression was converting equations slightly
    differently so that made a couple of tests needlessly fail.
    
    I've also fixed a problem were due to the ClauseExpression change, code
    was being printed like:
    
    { true }
    
    instead of:
    
    true
    
    I've modified ClauseExpression so that if the clause only contains
    a single expression, it drops the {}'s.

--- trunk/mov/test/org/mov/parser/ExpressionTest.java
+++ trunk/mov/test/org/mov/parser/ExpressionTest.java
@@ -5,15 +5,15 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package org.mov.parser;
@@ -42,7 +42,7 @@ public class ExpressionTest extends TestCase {
     }
 
     public void testSimplify() {
-        // Not 
+        // Not
         assertEquals("true", simplify("not(false)"));
         assertEquals("false", simplify("not(not(false))"));
         assertEquals("true", simplify("not(not(not(false)))"));
@@ -53,7 +53,7 @@ public class ExpressionTest extends TestCase {
         assertEquals("x!=y", simplify("not(x==y)"));
         assertEquals("x==y", simplify("not(x!=y)"));
 
-        // Or 
+        // Or
         assertEquals("false", simplify("false or false"));
         assertEquals("true", simplify("false or true"));
         assertEquals("true", simplify("true or false"));
@@ -65,7 +65,7 @@ public class ExpressionTest extends TestCase {
         assertEquals("a", simplify("a or a"));
         assertEquals("a or b", simplify("a or b"));
 
-        // And 
+        // And
         assertEquals("false", simplify("false and false"));
         assertEquals("false", simplify("false and true"));
         assertEquals("false", simplify("true and false"));
@@ -77,27 +77,27 @@ public class ExpressionTest extends TestCase {
         assertEquals("true", simplify("a and a"));
         assertEquals("a and b", simplify("a and b"));
 
-        // / 
+        // /
         assertEquals("2", simplify("10/5"));
         assertEquals("1", simplify("x/x")); // Pragmatism over idealism
         assertEquals("0", simplify("0/x"));
         assertEquals("x", simplify("x/1"));
         assertEquals("x/y", simplify("x/y"));
 
-        // + 
+        // +
         assertEquals("15", simplify("5+10"));
         assertEquals("x", simplify("0+x"));
         assertEquals("x", simplify("x+0"));
         assertEquals("2*x", simplify("x+x"));
         assertEquals("2*(x+y)", simplify("(x+y)+(x+y)"));
 
-        // - 
+        // -
         assertEquals("-5", simplify("5-10"));
         assertEquals("x", simplify("x-0"));
         assertEquals("0", simplify("x-x"));
         assertEquals("x-y", simplify("x-y"));
 
-        // * 
+        // *
         assertEquals("50", simplify("10*5"));
         assertEquals("x", simplify("x*1"));
         assertEquals("x", simplify("1*x"));
@@ -105,48 +105,48 @@ public class ExpressionTest extends TestCase {
         assertEquals("0", simplify("0*x"));
         assertEquals("x*y", simplify("x*y"));
 
-        // < 
+        // <
         assertEquals("true", simplify("5<10"));
         assertEquals("false", simplify("11<10"));
         assertEquals("false", simplify("x<x"));
         assertEquals("x<y", simplify("x<y"));
 
-        // > 
+        // >
         assertEquals("true", simplify("10>5"));
         assertEquals("false", simplify("5>10"));
         assertEquals("false", simplify("x>x"));
         assertEquals("x>y", simplify("x>y"));
 
-        // <= 
+        // <=
         assertEquals("true", simplify("5<=10"));
         assertEquals("false", simplify("11<=10"));
         assertEquals("true", simplify("x<=x"));
         assertEquals("x<=y", simplify("x<=y"));
 
-        // >= 
+        // >=
         assertEquals("true", simplify("10>=5"));
         assertEquals("false", simplify("5>=10"));
         assertEquals("true", simplify("x>=x"));
         assertEquals("x>=y", simplify("x>=y"));
 
-        // == 
+        // ==
         assertEquals("true", simplify("5==5"));
         assertEquals("false", simplify("5==10"));
         assertEquals("true", simplify("x==x"));
         assertEquals("x==y", simplify("x==y"));
 
-        // != 
+        // !=
         assertEquals("true", simplify("5!=10"));
         assertEquals("false", simplify("5!=5"));
         assertEquals("false", simplify("x!=x"));
         assertEquals("x!=y", simplify("x!=y"));
 
-        // If 
+        // If
         assertEquals("a", simplify("if(true) {a} else {b}"));
         assertEquals("b", simplify("if(false) {a} else {b}"));
         assertEquals("b", simplify("if(a) {b} else {b}"));
-        assertEquals("if(a) {a} else {b}", simplify("if(a) {a} else {b}"));
-        assertEquals("if(a) {b} else {a}", simplify("if(not(a)) {a} else {b}"));
+        assertEquals("if(a)\n   a\nelse\n   b\n", simplify("if(a) {a} else {b}"));
+        assertEquals("if(a)\n   b\nelse\n   a\n", simplify("if(not(a)) {a} else {b}"));
 
         // Sqrt
         assertEquals("5", simplify("sqrt(25)"));
@@ -159,11 +159,11 @@ public class ExpressionTest extends TestCase {
     private Expression parse(String string) {
         try {
             Variables variables = new Variables();
-            variables.add("x", Expression.INTEGER_TYPE);
-            variables.add("y", Expression.INTEGER_TYPE);
-            variables.add("a", Expression.BOOLEAN_TYPE);
-            variables.add("b", Expression.BOOLEAN_TYPE);
-            variables.add("c", Expression.BOOLEAN_TYPE);
+            variables.add("x", Expression.INTEGER_TYPE, false);
+            variables.add("y", Expression.INTEGER_TYPE, false);
+            variables.add("a", Expression.BOOLEAN_TYPE, false);
+            variables.add("b", Expression.BOOLEAN_TYPE, false);
+            variables.add("c", Expression.BOOLEAN_TYPE, false);
 
             return Parser.parse(variables, string);
         }

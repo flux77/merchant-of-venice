46c546b9b293fb77706aa7c89f7ebef6c01c89ed
mhummel <mhummel@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2006-10-13 15:21:50 +0000

    Improved Support and Resistence Algorithm.

--- trunk/mov/src/org/mov/chart/graph/SupportAndResistenceGraph.java
+++ trunk/mov/src/org/mov/chart/graph/SupportAndResistenceGraph.java
@@ -22,6 +22,7 @@ import java.awt.*;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.HashMap;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -34,14 +35,34 @@ import org.mov.util.*;
 /**
  * Support and Resistence "graph". This graph draws single lines across common price occurrances. 
  *
+ * The support and resistence levels are determined by the most numerous troughs and peaks within the last year of data that have the greatest vertical separation and do not overlap.
+ *
+ *
  * NOTE: Experimental and work in progress.
-
  * @author Mark Hummel
  */
+
+
+
 public class SupportAndResistenceGraph extends AbstractGraph {
 
     private Graphable support;
     private Graphable resistence;
+
+    private double average;
+    private double variance;
+
+    /*
+  Fudge factors: These values seemed to work well; I didn't do any sort of exhaustive experiment or optimisation to determine them.
+
+  Length of trend: directionCount > 4
+  Overlap tolerance: 20
+  
+    */
+
+    private static final double delta = 0.02;
+    private static final int overlapTolerance = 20;
+    
 
     /**
      * Create a new support and Resistence graph.
@@ -131,18 +152,30 @@ public class SupportAndResistenceGraph extends AbstractGraph {
 	double[] peaks, troughs;
 	double peaksPriceData[] = new double[2];
 	double troughsPriceData[] = new double[2];
-	int peaksIndex = 0;
-	int troughsIndex = 0;
+	HashMap startPeaksPosn, endPeaksPosn;
+	HashMap startTroughsPosn, endTroughsPosn;	
 	ArrayList peaksCount = new ArrayList(values.length);
 	ArrayList troughsCount = new ArrayList(values.length);
+	int peaksIndex = 0;
+	int troughsIndex = 0;
+
+	int score = 0;
+	boolean upmove = false;
+	int directionCount = 0;
+
 	double prev, diff;		
+	int start1, end1, start2, end2;
 	Set xRange = source.getXRange();
 	Iterator iterator = xRange.iterator();
-	boolean upmove = false;
 	int i;
 
 	peaks = new double[values.length];
 	troughs = new double[values.length];
+
+	startPeaksPosn = new HashMap();
+	startTroughsPosn = new HashMap();
+	endPeaksPosn = new HashMap();
+	endTroughsPosn = new HashMap();
 
 	prev = 0.0;	
 	peaksPriceData[0] = 0.0;
@@ -150,34 +183,62 @@ public class SupportAndResistenceGraph extends AbstractGraph {
 	troughsPriceData[0] = 0.0;
 	troughsPriceData[1] = 0.0;
 	
-	// Get all the peaks and troughs
-
-	for (i = 0; i < values.length; i++) {
+	//Get the initial direction
+	for (i = values.length-1; i > 0; i--) {
+	    
 	    diff = values[i] - prev;
-	    if (i == 0) {
-		// hole in logic here, when current price = prev
-		if (values[0] - values[1] > 0) {
-		    upmove = true;
-		} else {
-		    upmove = false;
-		}
-	    }	    
-
-	    if (i > 0) {		
-		// previous price was end of downmove, hence trough
-		if (diff > 0 && !upmove) {		
-		    troughs[troughsIndex++] = prev;
-		    upmove = true;
-		}
-		// previous price was end of upmove, hence peak		
-		if (diff < 0 && upmove) {
-		    peaks[peaksIndex++] = prev;		    
-		    upmove = false;
-		}
+	    if (values[i] == values[i-1]) {
+		continue;
 	    }
-	    prev = values[i];
+	    if (values[0] - values[1] > 0) {
+		upmove = true;		    
+	    } else {
+		upmove = false;
+	    }		
+	    break;
+	}	
+
+	//Try to make levels relevant by only considering
+	//the last year's data
+	int stop = values.length - 365;
+	if (stop < 0) {
+	    stop = 0;
 	}
 
+	// Get all the peaks and troughs
+
+	for (i = values.length-1; i > stop; i--) {
+	    diff = values[i] - prev;
+
+	    // previous price was end of downmove, hence trough
+	    if (diff > delta && !upmove && directionCount > 4) {		
+		if (startTroughsPosn.get( new Double(prev)) == null) {
+		    startTroughsPosn.put(new Double(prev), new Integer(i));
+		}
+		endTroughsPosn.put( new Double(prev), new Integer(i));
+		troughs[troughsIndex++] = prev;		    
+		upmove = true;
+		
+		directionCount = 0;
+	    }
+	    // previous price was end of upmove, hence peak		
+	    else if (diff < -delta && upmove && directionCount > 4) {
+		
+		if (startPeaksPosn.get(new Double(prev)) == null) {
+		    startPeaksPosn.put(new Double(prev), new Integer(i));			
+		}
+		endPeaksPosn.put(new Double(prev) ,new Integer(i));
+		peaks[peaksIndex++] = prev;		    
+		upmove = false;
+		directionCount = 0;
+	    } 
+	    
+	    else {
+		directionCount++;
+	    }	    
+	    prev = values[i];
+	}
+	
 	Arrays.sort(peaks, 0, peaksIndex);
 	Arrays.sort(troughs, 0, troughsIndex);
 
@@ -185,26 +246,87 @@ public class SupportAndResistenceGraph extends AbstractGraph {
 	countOccurances(troughsCount, troughs, troughsIndex-1);       	
 
 	//Choose the most numerous peak and troughs 
+	start1 = start2 = 0;
+	end1 = end2 = Integer.MAX_VALUE;
+
+	boolean overlap = false;
+	i = 0;
 	
-	if (peaksCount.size() > 0) {
-	    peaksPriceData = (double[])peaksCount.get(peaksCount.size()-1);
-	} else {
-	    // no peak data 
-	}
-	if (troughsCount.size() > 0) {
-	    troughsPriceData = (double[])troughsCount.get(troughsCount.size()-1);
-	} else {
-	    // no trough data.
+	int scoreList[] = 
+	    new int[(Math.max(peaksCount.size(),troughsCount.size()))];
+	
+	/* Mark levels high which are the most numerous, have the greatest
+	   vertical separation and whose separation is the least.
+	*/
+
+	while (i < (Math.min(peaksCount.size(),
+			     troughsCount.size()))) {
+
+	    score = 0;
+	    	
+	    peaksPriceData = (double[])peaksCount.get(peaksCount.size()-i-1);
+	    start1 = ((Integer)startPeaksPosn.get(new Double(peaksPriceData[0]))).intValue();
+		
+	    
+	    end1 = ((Integer)endPeaksPosn.get(new Double(peaksPriceData[0]))).intValue();
+		
+	    troughsPriceData = (double[])troughsCount.get(troughsCount.size()-i-1);
+	    start2 = ((Integer)startTroughsPosn.get(new Double(troughsPriceData[0]))).intValue();
+	    
+	    end2 = ((Integer)endTroughsPosn.get(new Double(troughsPriceData[0]))).intValue();
+	    
+	    
+	    if (start1 - overlapTolerance > end2 ||
+		end1 < start2 - overlapTolerance) {
+		score += 80;
+		
+	    } else {
+		score -= 10;
+	    }	    
+	    
+	    score += (end1 - start1);
+
+	    diff = Math.abs(troughsPriceData[0] - peaksPriceData[0]);
+	    
+	    if (diff > Math.sqrt(variance)) {
+		score += 50;
+	    }
+
+	    if (diff > (2 * Math.sqrt(variance))) {
+		score += 60;
+	    }
+
+	    score += (int)diff;
+	    scoreList[i] = score;
+	    
+	    i++;
 	}
 
+
+	Arrays.sort(scoreList);	
+	i = scoreList.length - 1;
+	
+	if (i > peaksCount.size()-1) {
+	    i = peaksCount.size() - 1;
+	}
+	if (i > troughsCount.size()-1) {
+	    i = troughsCount.size() - 1;
+	}
+	
+	troughsPriceData = (double[])troughsCount.get(i);
+	peaksPriceData = (double[])peaksCount.get(i);
+	
+	double supportPrice = troughsPriceData[0];
+	double resistPrice = peaksPriceData[0];
+	
 	i = 0;
-	while (iterator.hasNext()) {
+	while (iterator.hasNext()) {   	    
 	    Comparable x = (Comparable)iterator.next();
-	    if (values[i] == peaksPriceData[0]) {
-		resistence.putY(x, new Double(values[i]));
-	    }
-	    if (values[i] == troughsPriceData[0]) {
-		support.putY(x, new Double(values[i]));
+	    
+	    if (values[i] == resistPrice ||
+		values[i] == supportPrice) {
+		resistence.putY(x, new Double(resistPrice));
+		support.putY(x, new Double(supportPrice));		
 	    }
 	    i++;
 	}
@@ -221,12 +343,18 @@ public class SupportAndResistenceGraph extends AbstractGraph {
 	int i, priceIndex = 0;
 	double prev;
 	double priceData[] = new double[2];
+
+	average = 0.0;
+	variance = 0.0;
 	
 	prev = 0.0;	
 	priceData[0] = 0.0;
 	priceData[1] = 0.0;
 
-	for (i = 0; i < length-3; i++) {
+	for (i = 0; i < length-3; i++) {	    
+	    average += values[i];
+	    
+	    
 	    if (prev != values[i]) {
 		if (i > 0) {
 		    double[] tmp = new double[2];
@@ -245,12 +373,21 @@ public class SupportAndResistenceGraph extends AbstractGraph {
 	    prev = values[i];
 	}
 
+	variance = 0.0;
+	average /= length-3;
+	for (i = 0; i< length-3; i++) {
+	    variance += (values[i] - average)*(values[i] - average);
+	}
+
+	variance /= length-3;
+
 	// Last price data may have been same as prev, so it won't
 	// have been added.       
 	if (length > 0 && values[i-1] == prev) {
 	    list.add(priceIndex, priceData);
 	}
 	
+	/* Add the average line to the list and make sure its on top */
 	Collections.sort(list, new Comparator() {
 		public int compare(Object a, Object b) {
 		    double[] v1, v2;

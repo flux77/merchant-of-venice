46f147d3e6582bd21841c1ccce9e6148380608bf
mhummel <mhummel@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2008-05-18 22:10:51 +0000

    Simplfied save/restore frames

--- mov/src/org/mov/prefs/settings/ModuleFrameSettingsReader.java
+++ mov/src/org/mov/prefs/settings/ModuleFrameSettingsReader.java
@@ -23,7 +23,9 @@ import org.mov.quote.SymbolFormatException;
 import org.mov.util.Locale;
 
 import java.io.InputStream;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.beans.XMLDecoder;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.util.Vector;
@@ -88,126 +90,12 @@ public class ModuleFrameSettingsReader {
 	if (settings == null) {
 	    settings = new ModuleFrameSettings();
 	}
-
 	
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-	
-        try {
-            DocumentBuilder builder = builderFactory.newDocumentBuilder();
-            Document document = builder.parse(stream);
-            Element moduleFrameSettings = (Element)document.getDocumentElement();
-            NamedNodeMap moduleFrameSettingsAttributes = moduleFrameSettings.getAttributes();
-            Node moduleFrameNameNode = moduleFrameSettingsAttributes.getNamedItem("name");
-	    
-            NodeList childNodes = moduleFrameSettings.getChildNodes();
-	    boolean readGeometry = false;
+	BufferedInputStream buffStream = new BufferedInputStream(stream);
+	XMLDecoder xStream = new XMLDecoder(buffStream);
+	Object result = xStream.readObject();
+	xStream.close();
 
-	    NodeList geometryNodes = moduleFrameSettings.getElementsByTagName("geometry");
-	    
-	    NodeList moduleNodes = moduleFrameSettings.getElementsByTagName("module");
-	    
-
-	    if (moduleNodes.getLength() <= 0 || moduleNodes.getLength() > 1) 
-		throw new ModuleSettingsParserException(Locale.getString("MODULE_SETTINGS_INCORRECT_MODULES_NUM"));				
-	    
-	    if (geometryNodes.getLength() <= 0 || geometryNodes.getLength() > 1) 
-		throw new ModuleSettingsParserException(Locale.getString("MODULE_SETTINGS_INCORRECT_GEOMETRY_NUM"));				
-	    
-	    if (!readGeometryData((Element)geometryNodes.item(0))) {
-		throw new ModuleSettingsParserException(Locale.getString("MODULE_SETTINGS_TOP_LEVEL_ERROR"));		
-
-	    }
-	    
-	    if (!readModuleData((Element)moduleNodes.item(0)))  {
-		throw new ModuleSettingsParserException(Locale.getString("MODULE_SETTINGS_TOP_LEVEL_ERROR"));		
-	    } else {
-		settings.setModuleSettings(moduleSettings);
-	    }
-	    
-	} catch (SAXException e) {
-	    throw new ModuleSettingsParserException(e.getMessage());
-	} catch(ParserConfigurationException e) {
-	    throw new ModuleSettingsParserException(e.getMessage());
-	}
-	
-	return settings;    
+	return (ModuleFrameSettings)result;
     }
-    
-    static boolean readGeometryData(Element data) throws ModuleSettingsParserException {
-
-	int height = 0, width = 0;
-	int xpos = 0, ypos = 0;		    
-
-	NodeList sizeElement = data.getElementsByTagName("size");
-	NodeList positionElement = data.getElementsByTagName("position");
-	
-	if (sizeElement.getLength() <= 0 || sizeElement.getLength() > 1) 
-	    throw new ModuleSettingsParserException("Wrong Number of size elements");
-	
-	if (positionElement.getLength() <= 0 || positionElement.getLength() > 1) 
-	    throw new ModuleSettingsParserException("Wrong Number of position elements");
-	
-	ModuleSettingsParserException mspe = new ModuleSettingsParserException("MODULE_SETTINGS_GEOMETRY_NOT_DEF");
-
-	String heightStr = XMLHelper.getValue(data, "size/height");
-	String widthStr = XMLHelper.getValue(data, "size/width");
-	String readXPos = XMLHelper.getValue(data, "position/xpos");
-	String readYPos = XMLHelper.getValue(data, "position/ypos");
-				  
-	Double tmpy = new Double(heightStr);
-	Double tmpx = new Double(widthStr);
-		
-	width = tmpx.intValue();
-	height = tmpy.intValue();
-
-	tmpx = new Double(readXPos);
-	tmpy = new Double(readYPos);
-	
-	xpos = tmpx.intValue();
-	ypos = tmpy.intValue();
-	
-
-	Rectangle rectangle = new Rectangle();				
-	rectangle.setBounds(xpos, ypos, width, height);
-	settings.setBounds(rectangle);
-	
-	return true;
-    }
-    
-    static boolean readModuleData(Element data) {
-	int type;
-	int group;
-		
-	String groupStr = XMLHelper.getAttribute(data, "group");
-	String typeStr = XMLHelper.getAttribute(data, "type");
-
-	Integer tmp = new Integer(groupStr);
-	group = tmp.intValue();
-	tmp = new Integer(typeStr);
-	type = tmp.intValue();
-
-	switch (type) {
-	case Settings.CHARTMODULE:
-	    moduleSettings = ChartModuleSettingsReader.read(data);
-	    break;
-	case Settings.WATCHSCREENMODULE:
-	    moduleSettings = WatchScreenSettingsReader.read(data);
-	    break;
-	case Settings.HELPMODULE:
-	    moduleSettings = HelpSettingsReader.read(data);
-	    break;
-	case Settings.PORTFOLIOMODULE:
-	    moduleSettings = PortfolioSettingsReader.read(data);
-	    break;
-	case Settings.PREFERENCESMODULE:
-	    moduleSettings = PreferencesSettingsReader.read(data);
-	    break;
-	default:	   
-	    return false;
-	}
-		
-	return true;
-    }
-    
 }
-

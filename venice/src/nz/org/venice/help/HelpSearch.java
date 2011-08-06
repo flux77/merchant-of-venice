0e00caa22c48eaf23f50d11aeec54c6b8d2339d4
bromc <bromc@37db7df6-1e4f-0410-8d4e-dbce481d32e2>
2011-08-06 14:46:20 +0000

    Fixed bug #3302556 Find in help highlights wrong text section.

--- venice/src/nz/org/venice/help/HelpSearch.java
+++ venice/src/nz/org/venice/help/HelpSearch.java
@@ -5,15 +5,15 @@
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 package nz.org.venice.help;
@@ -30,66 +30,59 @@ import nz.org.venice.prefs.settings.HelpModuleSettings;
 
 
 /**
- * This class implements the help search functionality for the {@link HelpPage} Venice. 
+ * This class implements the help search functionality for the {@link HelpPage} Venice.
  * @see HelpModule
  */
 public class HelpSearch  {
-    
+
     String searchText;
 
     public HelpSearch(String text) {
-		HTMLEditorKit.ParserCallback callback = 
+		HTMLEditorKit.ParserCallback callback =
 	    new HTMLEditorKit.ParserCallback () {
-		boolean listOpen = false;
 
 		/*
 		  Parse the HTML creating a text string which resembles closely
 		  as possible how the output appears in the EditorPane.
-		  
+
 		  When a search is done, the index of the text in the pane
 		  will be the same as the string created here.
 		 */
 
 		public void handleText(char[] data, int pos) {
-		    if (listOpen) {
-			for (int i = 0; i < data.length; i++) {
-			    searchText += data[i];
-			}
-			searchText += "\n";
-			listOpen = false;
-		    } else {
-			for (int i = 0; i < data.length; i++) {
-			    searchText += data[i];
-			}
-		    }
-		}
+                    for (int i = 0; i < data.length; i++) {
+                        searchText += data[i];
+                    }
+                }
+
 		public void handleSimpleTag(Tag t, MutableAttributeSet a,
 				       int pos) {
 		}
 		public void handleStartTag(Tag t, MutableAttributeSet a,
 				       int pos) {
 		    if (t == Tag.LI) {
-			searchText += "";
-			listOpen = true;
+			searchText += "\n";
 		    }
 		    if (t == Tag.P) {
 			searchText += "\n";
 		    }
 
+                    if (t == Tag.H2) {
+                        searchText += "\n";
+                    }
+
 		    if (t == Tag.H3) {
 			searchText += "\n";
 		    }
-		    
-		    if (t == Tag.UL) {			
-			searchText += "\n";
+
+		    if (t == Tag.UL) {
 		    }
 
 		    if (t == Tag.I) {
-			
 		    }
 		}
 
-		
+
 		//The last line of html text is newline
 		public void handleEndOfLineString(String eol) {
 		    searchText += "\n";
@@ -97,9 +90,9 @@ public class HelpSearch  {
 		public void handleComment(Tag t, MutableAttributeSet a,
 				       int pos) {
 		}
-		
+
 	    };
-	
+
 	try {
 	    searchText = "";
 
@@ -110,7 +103,7 @@ public class HelpSearch  {
 	} catch (java.io.IOException e) {
 	    System.out.println("ioexception: " + e);
 	}
-	
+
     }
 
     public int find(String searchTerm, int prevIndex) {
